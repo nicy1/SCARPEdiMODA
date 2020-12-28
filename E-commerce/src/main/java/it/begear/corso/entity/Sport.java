7c3fd4662e441aabe31ccf7ba4c9d7f4f4b4a93e
irenerosaria <39551204+irenerosaria@users.noreply.github.com>
2020-12-28 13:06:16 +0100

    modifica su scarpa. aggiunta codice e disponibilita

--- E-commerce/src/main/java/it/begear/corso/entity/Sport.java
+++ E-commerce/src/main/java/it/begear/corso/entity/Sport.java
@@ -3,10 +3,16 @@ package it.begear.corso.entity;
 public class Sport extends Scarpa {
 	
 	public Sport() {}
-	public Sport(int id, Genere gender, String descrizione, Colore color, Misura numero, String brand,
-			double costo) {
-		super(id, gender, descrizione, color, numero, brand, costo);
+	
+	
+	
+	public Sport(int id, String codice, Genere gender, String descrizione, Colore color, Misura numero, String brand,
+			double costo, int disponibilita) {
+		super(id, codice, gender, descrizione, color, numero, brand, costo, disponibilita);
+		// TODO Auto-generated constructor stub
 	}
+
+
 
 	@Override
 	public void getTacco() {
