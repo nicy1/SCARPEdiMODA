2aa2445358c49f2dc0d3cb6f9cb99d063c39d0f3
irenerosaria <39551204+irenerosaria@users.noreply.github.com>
2021-01-07 10:42:27 +0100

    modifica utente

--- E-commerce/src/main/java/it/begear/corso/entity/Utente.java
+++ E-commerce/src/main/java/it/begear/corso/entity/Utente.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package it.begear.corso.entity;
 
 import javax.persistence.Column;
@@ -10,7 +9,6 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "utenti")
 
-//commento
 public class Utente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -88,94 +86,4 @@ public class Utente {
 	}
       
 }
-=======
-package it.begear.corso.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-
-@Entity
-@Table(name = "utenti")
-public class Utente {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_utenti")
-	  private int id;
-	
-	@Column(name = "nome")
-      private String nome;
-	
-	@Column(name = "cognome")
-      private String cognome;
-	
-	@Column(name = "email")
-      private String email;
-	
-	@Column(name = "password")
-      private String password;
-	
-	@Column(name = "indirizzo")
-      private String indirizzo;
-      
-      private Carrello carrello;
-
-	public Utente( String nome, String cognome, String email, String password, String indirizzo) {
-		super();	
-		setNome(nome);
-		setCognome(cognome);
-		setEmail(email);
-		setPassword(password);
-		setIndirizzo(indirizzo);
-		carrello = new Carrello(id);
-	}
-	
-	
-	public int getId() {
-		return id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCognome() {
-		return cognome;
-	}
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getIndirizzo() {
-		return indirizzo;
-	}
-	public void setIndirizzo(String indirizzo) {
-		this.indirizzo = indirizzo;
-	}
-
-	public Carrello getCarrello() {
-		return carrello;
-	}
-
-	public void setCarrello(Carrello carrello) {
-		this.carrello = carrello;
-	}
-      
-}
->>>>>>> branch 'develop' of https://github.com/irenerosaria/ScarpeDiModa.git
