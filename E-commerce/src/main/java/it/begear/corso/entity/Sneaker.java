5bb29c1ead4390d8f06c9cd1482549a8f136203e
irenerosaria <39551204+irenerosaria@users.noreply.github.com>
2021-01-09 17:43:14 +0100

    modifiche classe scarpa e figli

--- E-commerce/src/main/java/it/begear/corso/entity/Sneaker.java
+++ E-commerce/src/main/java/it/begear/corso/entity/Sneaker.java
@@ -2,22 +2,13 @@ package it.begear.corso.entity;
 
 public class Sneaker extends Scarpa {
 	
-	public Sneaker() {}
-	
-	
+	private static String descrizione="sneaker";
 
-	public Sneaker(int id, String codice, Genere gender, String descrizione, Colore color, Misura numero, String brand,
+	public Sneaker(String codice, Genere gender, Colore color, Misura numero, String brand,
 			double costo, int disponibilita) {
-		super(id, codice, gender, descrizione, color, numero, brand, costo, disponibilita);
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	@Override
-	public void getTacco() {
-		System.out.println("Le Sneaker non hanno il tacco");
+		super(codice, gender, descrizione, color, numero, brand, costo, disponibilita);
 		
 	}
+
 
 }
--- E-commerce/src/main/java/it/begear/corso/entity/Sport.java
+++ E-commerce/src/main/java/it/begear/corso/entity/Sport.java
@@ -2,22 +2,12 @@ package it.begear.corso.entity;
 
 public class Sport extends Scarpa {
 	
-	public Sport() {}
-	
-	
-	
-	public Sport(int id, String codice, Genere gender, String descrizione, Colore color, Misura numero, String brand,
+	private static String descrizione="sport";
+	public Sport( String codice, Genere gender, Colore color, Misura numero, String brand,
 			double costo, int disponibilita) {
-		super(id, codice, gender, descrizione, color, numero, brand, costo, disponibilita);
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	@Override
-	public void getTacco() {
-		System.out.println("Le scarpe sportive non hanno tacco");
+		super( codice, gender, descrizione, color, numero, brand, costo, disponibilita);
 		
 	}
+
 
 }
